a7e999e7c910228ab4433b049b7a8527a8730aee
Guilherme <guilhermeds822@gmail.com>
2026-04-18 17:19:03 -0300

    Concluído o JavaScript e sistema funcionado, de maneira simplista

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+    let telefone = document.getElementById("telefone").value;
+
+    if (!nome || !email || !senha || !telefone) {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    alert("Cadastro realizado com sucesso!");
+});
